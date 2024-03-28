489dd30997bfe1123c22342fabf5816c35b9d4db
BraylonHigginbotham <brh00003@mix.wvu.edu>
2024-03-28 19:46:38 -0400

    Pushed 2 stored procsturened into api's

---
+++ Assignment2_EcoInvest_BrandonBiggins/EcoInvestAPI/Controllers/SearchCompaniesByGeoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EcoInvestAPI.Entities;
+using EcoInvestAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+//Braylon Higginbotham
+namespace EcoInvestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchCompaniesByGeoController : ControllerBase
+    {
+        private readonly ISearchCompaniesByGeo _viewSearchCompaniesByGeo;
+
+        public SearchCompaniesByGeoController(ISearchCompaniesByGeo viewSearchCompaniesByGeo)
+        {
+            _viewSearchCompaniesByGeo = viewSearchCompaniesByGeo;
+        }
+
+        [HttpGet("{CompanyLongitude}/{CompanyLatitude}")]
+        public async Task<IActionResult> ViewSearchCompaniesByGeo(string CompanyLongitude, string CompanyLatitude)
+        {
+            var companyLatandLong = await _viewSearchCompaniesByGeo.SearchCompaniesByGeo(CompanyLongitude, CompanyLatitude);
+
+            if (companyLatandLong == null || companyLatandLong.Count == 0)
+            {
+                return Ok(companyLatandLong);
+            }
+
+            return NotFound();
+        }
+    }
+}
+
+
+
---
+++ Assignment2_EcoInvest_BrandonBiggins/EcoInvestAPI/Repositories/AddClimateAndStockData.cs
@@ -0,0 +1,35 @@
+﻿using EcoInvestAPI.Data;
+using EcoInvestAPI.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Threading.Tasks;
+//Braylon Higginbotham
+
+namespace EcoInvestAPI.Repositories
+{
+    public class AddClimateAndStockDataController : IAddClimateAndStockData
+    {
+        private readonly DbContextClass _dbContextClass;
+
+        public AddClimateAndStockDataController(DbContextClass dbContextClass)
+        {
+            this._dbContextClass = dbContextClass;
+        }
+
+        public async Task<List<Company>> AddClimateAndStockData(int CompanyID, DateTime DateValue, double AvgTemperature)
+        {
+
+            var comp = new SqlParameter("@CompanyID", CompanyID);
+            var Datavalue = new SqlParameter("@DateValue", DateValue);
+            var Temp = new SqlParameter("@AvgTemperature", AvgTemperature);
+
+            var viewAddClimateAndStockData = await _dbContextClass.Company
+                .FromSqlRaw("EXEC AddClimateAndStockData @CompanyID = 1,    @DateValue = \"2023-01-20\",    @AvgTemperature = 79.44,;", comp, Datavalue, Temp)
+                .ToListAsync();
+            return viewAddClimateAndStockData;
+        }
+    }
+}
