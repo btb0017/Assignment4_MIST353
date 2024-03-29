050c9f6b67d1add3bbdb182dd75d686c430f9b65
ryanbankert <rjb00014@mix.wvu.edu>
2024-03-28 21:47:56 -0400

    Pushing CompanyRating and CompanyAdd

--- Assignment2_EcoInvest_BrandonBiggins/EcoInvestAPI/Repositories/ICompanyAddService.cs
+++ Assignment2_EcoInvest_BrandonBiggins/EcoInvestAPI/Repositories/ICompanyAddService.cs
@@ -1,9 +1,9 @@
 ﻿using EcoInvestAPI.Entities;
-
+//Ryan Bankert
 namespace EcoInvestAPI.Repositories
 {
     public interface ICompanyAddService
     {
-        
+        public Task<int> AddCompany(Company company);
     }
 }
