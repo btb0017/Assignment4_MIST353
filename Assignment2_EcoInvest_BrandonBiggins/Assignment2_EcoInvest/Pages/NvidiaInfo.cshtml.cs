e71121d38ff23c64021f5262061e981b650cde16
ryanbankert <rjb00014@mix.wvu.edu>
2024-04-09 21:05:15 -0400

    Updated/pushed company pages

---
+++ Assignment2_EcoInvest_BrandonBiggins/Assignment2_EcoInvest/Pages/NvidiaInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment2_EcoInvest.Pages
+{
+    public class NvidiaInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
